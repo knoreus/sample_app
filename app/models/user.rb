1bc12634bce4bc8a7e5972b65864c4ac9d20f670
Kevin Noreus <knoreus@gmail.com>
2011-11-13 11:26:42 -0700

    Done with user edit/update, index, an destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@
 
 class User < ActiveRecord::Base
   attr_accessor :password
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation, :admin
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
